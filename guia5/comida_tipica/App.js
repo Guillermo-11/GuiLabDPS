3ee8b52dad34b390c37009d798f12ad9f7fc087f
Guillermo Granadino <gr222755@alumno.udb.edu.sv>
2025-08-11 15:49:49 -0600

    G5 - Creación de Apk Cars

--- guia5/comida_tipica/App.js
+++ guia5/comida_tipica/App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, SafeAreaView, StatusBar, Image } from 'react-native';
+import { Card, Button, Icon } from '@rneui/themed';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Card>
+      <Card.Title>Comida Típica</Card.Title>
+      <Card.Divider/>
+      <Text>Este es un ejemplo básico de una tarjeta.</Text>
+    </Card>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
